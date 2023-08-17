17df28ecce58e413208bab14d0e9c65474240586
Sergio Felix <sfelix@google.com>
2023-08-17 12:11:17 -0700

    Added @Param

--- src/main/java/com/example/spannercatservice/CatRepository.java
+++ src/main/java/com/example/spannercatservice/CatRepository.java
@@ -4,10 +4,11 @@ import com.google.cloud.spanner.Key;
 import com.google.cloud.spring.data.spanner.repository.SpannerRepository;
 import com.google.cloud.spring.data.spanner.repository.query.Query;
 import java.util.List;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CatRepository extends SpannerRepository<Cat, Key> {
   @Query("SELECT DISTINCT name, lastName FROM CATS WHERE name = @name")
-  List<CatProjection> findDistinctCatsByNameAndLastName(String name);
+  List<CatProjection> findDistinctCatsByNameAndLastName(@Param("name") String name);
 }
